c1a64d5092acd53f0aa43631cef0ed2d90cf5132
snakeice45 <jonas171192@gmail.com>
2019-11-04 23:55:04 -0600

    Counting Valleys commentary and process added

--- src/com/countingvalleys/app/CountingValleys.java
+++ src/com/countingvalleys/app/CountingValleys.java
@@ -3,12 +3,16 @@ package com.countingvalleys.app;
 public class CountingValleys {
 
 	public static void main(String[] args) {
+		//https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+		//hacker rank input
 		String  str = "DDUUDDUDUUUD";
+		
+		//declarations
 		char[] chars = str.toCharArray();
 		int count1 = 0;
 		int valleyCount=0;
 		
-		
+		//get the valleys by coords
 		for (int i = 0; i < chars.length; i++) {
 			if(chars[i] == 'D')
 			{
@@ -27,7 +31,7 @@ public class CountingValleys {
 			
 		}
 		
-		System.out.println(valleyCount);
+		System.out.println("Valleys passed: "+valleyCount);
 	}
 
 }
